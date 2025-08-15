c4f4512a55df26a40dda13f9deabec1b09bb8efc
jose vitor <josevitorrodrigues17@gmail.com>
2025-08-15 17:42:53 -0300

    feat: add filters

--- src/config/httpException/http-exception.filter.ts
+++ src/config/httpException/http-exception.filter.ts
@@ -2,18 +2,30 @@ import {
   ExceptionFilter,
   Catch,
   ArgumentsHost,
-  HttpException
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { Response } from 'express';
 
-@Catch(HttpException)
+@Catch(HttpException, Error)
 export class HttpExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: HttpException | Error, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
 
-    response
-      .status(exception.getStatus())
-      .json(exception.getResponse());
+    if (exception instanceof HttpException) {
+      response
+        .status(exception.getStatus())
+        .json(exception.getResponse());
+    } else {
+      // Handle unexpected errors
+      response
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Internal server error',
+          error: process.env.NODE_ENV === 'development' ? exception.message : undefined
+        });
+    }
   }
 }
